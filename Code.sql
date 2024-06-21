3eea9311fe82fbba40311c6b4eab76a31b14ad26
manasa roopa <59734951+manasaroopa@users.noreply.github.com>
2024-06-21 11:30:37 +0530

    Update Code.sql

--- Code.sql
+++ Code.sql
@@ -1,5 +1,29 @@
 use jb_analysis;
 
+select ds as dates, round(3600/sum(time_spent)*count(ds))
+as jobs_reviewed_per_hour
+from job_data
+group by ds;
+
+
+
+SELECT 
+COUNT(event) / SUM(time_spent)
+FROM
+job_data;
+
+SELECT 
+    language,
+    COUNT(language) * 100 / (SELECT 
+            COUNT(*)
+        FROM
+            job_data)
+FROM
+    job_data
+GROUP BY language;
+
+
+
 select EXTRACT(WEEK FROM occurred_at) AS "week number" , 
 count(distinct user_id) as "active users"
 from events
