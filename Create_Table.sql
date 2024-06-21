d14a39c27fc5f63ee3e590404ec8f2fad9f32ddd
manasa roopa <59734951+manasaroopa@users.noreply.github.com>
2024-06-21 12:04:25 +0530

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -7,6 +7,7 @@ location varchar(50),
 device varchar(50),
 user_type int);
 
+-- adding data from source data file
 load data infile "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/events.csv"
 into table events
 fields terminated by ','
@@ -18,3 +19,55 @@ alter table events add column temp_occurred_at datetime;
 update events set temp_occurred_at = str_to_date(occurred_at, '%d-%m-%Y %H:%i');
 alter table events drop column occurred_at;
 alter table events change column temp_occurred_at occurred_at datetime;
+
+
+
+CREATE TABLE users(
+	user_id INT,
+	created_at TIMESTAMP,
+	company_id INT,
+	language VARCHAR(100),
+	activated_at TIMESTAMP,
+	state VARCHAR(50)
+	);
+
+
+COPY users 
+FROM 'D:\Operational-Analytics-Study\src\data\users.csv'
+WITH (FORMAT CSV, HEADER);
+
+
+CREATE TABLE email_events(
+	user_id INT,
+	occured_at TIMESTAMP,
+	action VARCHAR(100),
+	user_type INT
+	
+	);
+
+--Adding Data in users table
+
+COPY email_events 
+FROM 'D:\Operational-Analytics-Study\src\data\email_events.csv'
+WITH (FORMAT CSV, HEADER);
+
+
+CREATE TABLE job_data(
+	ds DATE,
+	job_id INT,
+	actor_id INT,
+	event VARCHAR(30),
+	language VARCHAR(30),
+	time_spent INT,
+	org VARCHAR(30)
+	
+	);
+SET datestyle = 'ISO, MDY';
+--Adding Data in users table
+
+COPY job_data 
+FROM 'D:\Operational-Analytics-Study\src\data\job_data.csv'
+WITH (FORMAT CSV, HEADER);
+
+--Checking for users table
+SELECT * FROM job_data ;
